e8b9ec5b706f96cba29acffc054a2430d18ed363
SENGHAK.CHHUN <SENGHAK.CHHUN@pnc.lan>
2024-04-08 19:37:29 +0700

    add file

---
+++ practice/customer.ts
@@ -0,0 +1,14 @@
+import { Address } from "./address";
+export class Customer{
+    firstName : string;
+    lastName : string
+    address : Address[] = [];
+    constructor(firstName : string, lastName : string){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    setAddress(address : Address){
+        this.address.push(address)
+    }
+    
+}
